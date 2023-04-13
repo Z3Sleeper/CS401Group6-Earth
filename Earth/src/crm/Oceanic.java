1c7e9010ecc69637940376449f01bc178c72f415
MartinezMac <SalvadorMtz421@gmail.com>
2023-04-12 19:15:53 -0700

    Oceanic Climate Added

---
+++ Earth/src/crm/Oceanic.java
@@ -0,0 +1,14 @@
+package crm;
+
+public class Oceanic extends Climate{
+
+    public Oceanic() {
+        name = "      Oceanic      ";
+        VP_effect = "3";
+        climate1 = "Sunny";
+        climate2 = "Wet";
+        event1 = "-1 Growth";
+        event2 = "+2 Soil";
+
+    }
+}
