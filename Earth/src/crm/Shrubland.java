90860400cf00c490b49a100aa963a66b2dbb4ff5
ErmakovGeorge <98370485+ErmakovGeorge@users.noreply.github.com>
2023-04-14 08:34:30 -0700

    Terrain class is created

---
+++ Earth/src/crm/Shrubland.java
@@ -0,0 +1,5 @@
+package crm;
+
+public class Shrubland {
+    
+}
--- Earth/src/crm/YellowBelliedMarmot.java
+++ Earth/src/crm/YellowBelliedMarmot.java
@@ -8,5 +8,23 @@ public class YellowBelliedMarmot extends Fauna {
         objective2 = " Each with 4+ sprouts"; 
         objective3 = "";
     }
-    
+     //FIXME:
+    /*public int cardEffect(PlayerBoard p) { 
+        int count = 0; 
+        for (int i=0; i< p.getBoardSize(); i++) {
+            for (int j = 0; j < p.getBoardSize(); j++) {
+                
+                // FIXME: Once we have Flora, change this line to:
+                //  p.getPlayGrid()[i][j].getType() == (string for Flora's type, ex: "     <Flora>")
+                // Then add another condition for the "filled" part described above.
+                if (p.getPlayGrid()[i][j].getName() != "placeholder"){
+                    count++;
+                }
+            }
+            if (count == 4) {
+                return 1;
+            }
+        }
+        return 0;
+    } */
 }
